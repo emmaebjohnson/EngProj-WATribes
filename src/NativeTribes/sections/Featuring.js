6191dd01bb571fd90151e505c8b71c4655c280e0
emma <97130396+emmaebjohnson@users.noreply.github.com>
2024-02-25 18:27:14 -0800

    added featuring.css and fixed dynamic graph

--- src/NativeTribes/sections/Featuring.js
+++ src/NativeTribes/sections/Featuring.js
@@ -1,7 +1,6 @@
-import Container from 'assets/theme/components/container';
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import './Featuring.css'
 const NativePopulationBarGraph = () => {
   const data = [
     { name: 'OK', percentage: 14.2 },
@@ -12,65 +11,61 @@ const NativePopulationBarGraph = () => {
   ];
 
   return (
-    <MKBox>
-      <Container>
     <div>
       <h1 style={{ textAlign: 'center' }}>% of the Native Population in Each State</h1>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div style={{ display: 'inline-block', marginRight: '20px' }}>
+      <div className="chart-container">
+        <div className="chart">
           <h2 style={{ textAlign: 'center' }}>American Indian</h2>
-          <BarChart
-            width={400} // Adjust width as needed
-            height={400} // Adjust height as needed
-            data={data}
-            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="percentage" fill="#8884d8" name="% of Population" />
-          </BarChart>
+          <ResponsiveContainer width="100%" aspect={2}>
+            <BarChart
+              data={data}
+              margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="percentage" fill="#8884d8" name="% of Population" />
+            </BarChart>
+          </ResponsiveContainer>
         </div>
-        <div style={{ display: 'inline-block', marginRight: '20px' }}>
+        <div className="chart">
           <h2 style={{ textAlign: 'center' }}>Alaska Native</h2>
-          <BarChart
-            width={400} // Adjust width as needed
-            height={400} // Adjust height as needed
-            data={data}
-            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="percentage" fill="#82ca9d" name="% of Population" />
-          </BarChart>
+          <ResponsiveContainer width="100%" aspect={2}>
+            <BarChart
+              data={data}
+              margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="percentage" fill="#82ca9d" name="% of Population" />
+            </BarChart>
+          </ResponsiveContainer>
         </div>
-        <div style={{ display: 'inline-block' }}>
+        <div className="chart">
           <h2 style={{ textAlign: 'center' }}>Latin American Indian</h2>
-          <BarChart
-            width={400} // Adjust width as needed
-            height={400} // Adjust height as needed
-            data={data}
-            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="percentage" fill="#ffc658" name="% of Population" />
-          </BarChart>
+          <ResponsiveContainer width="100%" aspect={2}>
+            <BarChart
+              data={data}
+              margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="percentage" fill="#ffc658" name="% of Population" />
+            </BarChart>
+          </ResponsiveContainer>
         </div>
       </div>
       <p style={{ marginTop: '20px' }}>Over half (50.9%) of the American Indian alone population lived in five states.
        <br></br>*data taken from <a href="https://www.census.gov/library/stories/2023/10/2020-census-dhc-a-aian-population.html">https://www.census.gov/library/stories/2023/10/2020-census-dhc-a-aian-population.html</a></p>
     </div>
-    </Container>
-    </MKBox>
   );
 };
 
