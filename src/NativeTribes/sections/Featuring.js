78cdca6bcd58be10829e0cb5cc276841c6c10224
emma <97130396+emmaebjohnson@users.noreply.github.com>
2024-02-26 21:33:39 -0800

    fixed graph data

--- src/NativeTribes/sections/Featuring.js
+++ src/NativeTribes/sections/Featuring.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './Featuring.css'
 const NativePopulationBarGraph = () => {
-  const data = [
+  const America = [
     { name: 'OK', percentage: 14.2 },
     { name: 'AZ', percentage: 12.9 },
     { name: 'CA', percentage: 9.9 },
     { name: 'NM', percentage: 9.1 },
     { name: 'TX', percentage: 4.8 },
+  ];
+  const Alaska = [
+    { name: 'AK', percentage: 77.1 },
+    { name: 'WA', percentage: 6.0 },
+    { name: 'CA', percentage: 2.5 },
+    { name: 'OR', percentage: 1.8 },
+    { name: 'TX', percentage: 1.4 },
+  ];
+  const Latin = [
+    { name: 'CA', percentage: 32.7 },
+    { name: 'TX', percentage: 11.6 },
+    { name: 'NY', percentage: 8.1 },
+    { name: 'IL', percentage: 6.3 },
+    { name: 'FL', percentage: 4.6 },
   ];
 
   return (
@@ -18,7 +32,7 @@ const NativePopulationBarGraph = () => {
           <h2 style={{ textAlign: 'center' }}>American Indian</h2>
           <ResponsiveContainer width="100%" aspect={2}>
             <BarChart
-              data={data}
+              data={America}
               margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
             >
               <CartesianGrid strokeDasharray="3 3" />
@@ -34,7 +48,7 @@ const NativePopulationBarGraph = () => {
           <h2 style={{ textAlign: 'center' }}>Alaska Native</h2>
           <ResponsiveContainer width="100%" aspect={2}>
             <BarChart
-              data={data}
+              data={Alaska}
               margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
             >
               <CartesianGrid strokeDasharray="3 3" />
@@ -50,7 +64,7 @@ const NativePopulationBarGraph = () => {
           <h2 style={{ textAlign: 'center' }}>Latin American Indian</h2>
           <ResponsiveContainer width="100%" aspect={2}>
             <BarChart
-              data={data}
+              data={Latin}
               margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
             >
               <CartesianGrid strokeDasharray="3 3" />
